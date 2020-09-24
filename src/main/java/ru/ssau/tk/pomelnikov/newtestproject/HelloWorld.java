9c9565aff859352fe364e555b45e7465388b1a10
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-09-24 20:51:18 +0400

    Finally made objects of classes

--- src/main/java/ru/ssau/tk/pomelnikov/newtestproject/HelloWorld.java
+++ src/main/java/ru/ssau/tk/pomelnikov/newtestproject/HelloWorld.java
@@ -2,6 +2,22 @@ package ru.ssau.tk.pomelnikov.newtestproject;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        //System.out.println("Hello, world!");
+
+        Person firstPerson = new Person();
+        firstPerson.setFirstName("Mark");
+        firstPerson.setLastName("Kierhe");
+        firstPerson.setPassportId(1234);
+
+        Person secondPerson = new Person();
+        secondPerson.setFirstName("Aleksandr");
+        secondPerson.setLastName("Bassilica");
+        secondPerson.setPassportId(5678);
+
+        Person thirdPerson =  new Person("Gans", "von Stromberg");
+
+        Person fourthPerson = new Person (1596);
+
+        Person fifthPerson = new Person ("Filipp", "August", 6688);
     }
 }
