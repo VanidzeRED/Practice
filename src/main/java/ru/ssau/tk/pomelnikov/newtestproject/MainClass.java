69afda1ac8bdc3e67a9aab5260570d1bc020e8f2
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-09-26 11:21:08 +0400

    Update MainClass.java
    В методе checkAnotherPerson создаётся новый объект класса Person, его поле и выводится первым, полеобъекта из класса main остаётся неизменным, его мы и видем во второй строке вывода

--- src/main/java/ru/ssau/tk/pomelnikov/newtestproject/MainClass.java
+++ src/main/java/ru/ssau/tk/pomelnikov/newtestproject/MainClass.java
@@ -4,20 +4,13 @@ public class MainClass {
     public static void main(String[] args) {
         Person person = new Person();
         person.setFirstName("Arkadiy");
-        int number = 5;
-        checkInt(number);
-        checkPerson(person);
-        System.out.println(number);
+        checkAnotherPerson(person);
         System.out.println(person.getFirstName());
     }
 
-    private static void checkInt(int number) {
-        number = 10;
-        System.out.println(number);
-    }
-
-    private static void checkPerson(Person person) {
-        person.setFirstName("Oleg");
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();
+        person.setFirstName("Ignat");
         System.out.println(person.getFirstName());
     }
 
