29640249e902157c623f1fb137b949c181e9d1bf
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2020-11-10 16:43:23 +0400

    Finished cutLong

--- src/test/java/ru/ssau/tk/pomelnikov/newtestproject/ArraysTest.java
+++ src/test/java/ru/ssau/tk/pomelnikov/newtestproject/ArraysTest.java
@@ -8,7 +8,7 @@ public class ArraysTest {
     Arrays arrayTest = new Arrays();
 
     @Test
-    private void setArrayTest() {
+    public void setArrayTest() {
         for (int iterator = 1; iterator <= 10; iterator++) {
             double[] currentArray = new double[iterator];
             assertEquals(arrayTest.setArray(iterator), currentArray);
@@ -16,8 +16,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void fillArrayOneTest() {
-
+    public void fillArrayOneTest() {
         assertEquals(arrayTest.fillArrayOne(4), new int[]{2, 1, 1, 2});
         assertEquals(arrayTest.fillArrayOne(5), new int[]{2, 1, 1, 1, 2});
         assertEquals(arrayTest.fillArrayOne(6), new int[]{2, 1, 1, 1, 1, 2});
@@ -25,7 +24,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void fillArrayOddTest() {
+    public void fillArrayOddTest() {
         assertEquals(arrayTest.fillArrayOdd(1), new int[]{1});
         assertEquals(arrayTest.fillArrayOdd(3), new int[]{1, 3, 5});
         assertEquals(arrayTest.fillArrayOdd(4), new int[]{1, 3, 5, 7});
@@ -34,7 +33,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void fillArrayEvenTest() {
+    public void fillArrayEvenTest() {
         assertEquals(arrayTest.fillArrayEven(1), new int[]{2});
         assertEquals(arrayTest.fillArrayEven(3), new int[]{6, 4, 2});
         assertEquals(arrayTest.fillArrayEven(4), new int[]{8, 6, 4, 2});
@@ -43,7 +42,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void fillArrayFibonachTest() {
+    public void fillArrayFibonachTest() {
         assertEquals(arrayTest.fillArrayFibonachi(1), new int[]{1});
         assertEquals(arrayTest.fillArrayFibonachi(3), new int[]{1, 1, 2});
         assertEquals(arrayTest.fillArrayFibonachi(4), new int[]{1, 1, 2, 3});
@@ -51,7 +50,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void fillArraySqrTest() {
+    public void fillArraySqrTest() {
         assertEquals(arrayTest.fillArraySqr(3), new double[]{0., 1., 4.});
         assertEquals(arrayTest.fillArraySqr(4), new double[]{0., 1., 4., 9.});
         assertEquals(arrayTest.fillArraySqr(6), new double[]{0., 1., 4., 9., 16., 25.});
@@ -59,7 +58,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void fillArrayNaturalTest() {
+    public void fillArrayNaturalTest() {
         assertEquals(arrayTest.fillArrayNatural(3), new int[]{1, 2, 4});
         assertEquals(arrayTest.fillArrayNatural(4), new int[]{1, 2, 4, 5});
         assertEquals(arrayTest.fillArrayNatural(6), new int[]{1, 2, 4, 5, 7, 8});
@@ -68,7 +67,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void changeToNegative() {
+    public void changeToNegative() {
         final int[] testArray1 = {-1, 0, 1};
         final int[] testArray2 = {9, -9};
         arrayTest.changeToNegative(testArray1);
@@ -81,7 +80,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void createDoubleDimentionArrayTest() {
+    public void createDoubleDimentionArrayTest() {
         int[][] resultArray1 = arrayTest.createDoubleDimentionArray(3);
         int[][] resultArray2 = arrayTest.createDoubleDimentionArray(4);
 
@@ -96,21 +95,21 @@ public class ArraysTest {
     }
 
     @Test
-    private void fillArrayArithmeticProgressionTest() {
+    public void fillArrayArithmeticProgressionTest() {
         assertEquals(arrayTest.fillArrayArithmeticProgression(5, 0, 1), new double[]{0., 1., 2., 3., 4.});
         assertEquals(arrayTest.fillArrayArithmeticProgression(5, -0.5, 0.5), new double[]{-0.5, 0., 0.5, 1., 1.5});
         assertEquals(arrayTest.fillArrayArithmeticProgression(5, 1.6, -0.8), new double[]{1.6, 0.8, 0, -0.8, -1.6});
     }
 
     @Test
-    private void fillArrayGeometricProgressionTest() {
+    public void fillArrayGeometricProgressionTest() {
         assertEquals(arrayTest.fillArrayGeometricProgression(5, 1, 2), new double[]{1., 2., 4., 8., 16.});
         assertEquals(arrayTest.fillArrayGeometricProgression(5, 2, -2), new double[]{2., -4., 8., -16., 32.});
         assertEquals(arrayTest.fillArrayGeometricProgression(4, 1, 0.5), new double[]{1., 0.5, 0.25, 0.125});
     }
 
     @Test
-    private void findDenominatorsTest() {
+    public void findDenominatorsTest() {
         assertEquals(arrayTest.findDenominators(2), new int[]{1, 2});
         assertEquals(arrayTest.findDenominators(18), new int[]{1, 2, 3});
         assertEquals(arrayTest.findDenominators(27), new int[]{1, 3});
@@ -118,7 +117,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void testIsPrime() {
+    public void testIsPrime() {
         assertTrue(Arrays.isPrime(2));
         assertTrue(Arrays.isPrime(5));
         assertTrue(Arrays.isPrime(13));
@@ -128,21 +127,21 @@ public class ArraysTest {
     }
 
     @Test
-    private void testFindPrime() {
+    public void testFindPrime() {
         assertEquals(arrayTest.findPrime(10), new int[]{2, 3, 5, 7});
         assertEquals(arrayTest.findPrime(23), new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23});
         assertEquals(arrayTest.findPrime(2), new int[]{2});
     }
 
     @Test
-    private void testFillArrayMirror() {
+    public void testFillArrayMirror() {
         assertEquals(arrayTest.fillArrayMirror(7), new int[]{1, 2, 3, 4, 3, 2, 1});
         assertEquals(arrayTest.fillArrayMirror(2), new int[]{1, 1});
         assertEquals(arrayTest.fillArrayMirror(8), new int[]{1, 2, 3, 4, 4, 3, 2, 1});
     }
 
     @Test
-    private void testIsInArray() {
+    public void testIsInArray() {
         int[] array = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         assertTrue(arrayTest.isInArray(array, 0));
         assertTrue(arrayTest.isInArray(array, 3));
@@ -153,7 +152,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void testIsNullInArray() {
+    public void testIsNullInArray() {
         Integer[] array1 = new Integer[]{1, 2, 3};
         Integer[] array2 = new Integer[]{1, null, 3};
         Integer[] array3 = new Integer[]{null, null};
@@ -164,49 +163,49 @@ public class ArraysTest {
     }
 
     @Test
-    private void testFindNumberOfEven() {
+    public void testFindNumberOfEven() {
         assertEquals(arrayTest.findCountOfEven(new int[]{1, 2, 1, 2, 1}), 2);
         assertEquals(arrayTest.findCountOfEven(new int[]{1, 3, 5, 7, 9}), 0);
         assertEquals(arrayTest.findCountOfEven(new int[]{1, 2, 4, 6, 10}), 4);
     }
 
     @Test
-    private void testFindMax() {
+    public void testFindMax() {
         assertEquals(arrayTest.findMax(new int[]{1, 2, 3, 4, 3, 2, 1}), Integer.valueOf(4));
         assertEquals(arrayTest.findMax(new int[]{5, -2, -3, -4, -3, 10, -1}), Integer.valueOf(10));
         assertNull(arrayTest.findMax(new int[]{}));
     }
 
     @Test
-    private void testFindMin() {
+    public void testFindMin() {
         assertEquals(arrayTest.findMin(new int[]{1, 2, 3, 4, 3, 2, 1}), Integer.valueOf(1));
         assertEquals(arrayTest.findMin(new int[]{5, -2, -3, -4, -3, 10, -1}), Integer.valueOf(-4));
         assertNull(arrayTest.findMin(new int[]{}));
     }
 
     @Test
-    private void testFindSum() {
+    public void testFindSum() {
         assertEquals(arrayTest.findSum(new int[]{1, 0, 1, 0, 1, 0}), 3);
         assertEquals(arrayTest.findSum(new int[]{0, 1, 0, 1, 0, 1, 0}), 0);
         assertEquals(arrayTest.findSum(new int[]{9, 13, 5, 6, 9, 7, 32, 6, 10, 5, 6, 46, 5}), 76);
     }
 
     @Test
-    private void testIsMultiple() {
+    public void testIsMultiple() {
         assertTrue(arrayTest.isMultiple(new int[]{2, 4, 6, 9, 10, 3}));
         assertTrue(arrayTest.isMultiple(new int[]{10, 100, 50, 58, 60, 3}));
         assertFalse(arrayTest.isMultiple(new int[]{10, 100, 50, 58, 60, 2}));
     }
 
     @Test
-    private void testFindMostPopular() {
+    public void testFindMostPopular() {
         assertEquals(arrayTest.findMostPopular(new int[]{1, 2, 3, 4, 1, 2}), 1);
         assertEquals(arrayTest.findMostPopular(new int[]{-1, 2, 2, 3, -1, -1, 2}), -1);
         assertEquals(arrayTest.findMostPopular(new int[]{1, 1, 2, 2, 2, 1}), 1);
     }
 
     @Test
-    private void testFindIndexOfElem() {
+    public void testFindIndexOfElem() {
         assertEquals(arrayTest.findIndexOfElem(new int[]{0, 1, 2, 3, 4, 5}, 1), 1);
         assertEquals(arrayTest.findIndexOfElem(new int[]{0, 1, 2, 3, 4, 5}, 3), 3);
         assertEquals(arrayTest.findIndexOfElem(new int[]{0, 1, 2, 3, 4, 5}, 5), 5);
@@ -214,7 +213,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void testExchangeMaxMin() {
+    public void testExchangeMaxMin() {
         int[] array1 = new int[]{1, 2, 3};
         int[] array2 = new int[]{1, 5, 3};
         int[] array3 = new int[]{-1, 0, 1, 4, 10, 5, 2};
@@ -227,7 +226,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void testByteNegate() {
+    public void testByteNegate() {
         int[] array = new int[]{-16, -8, -7, -2, -1, 0, 1, 2, 7, 8, 10, 32};
         arrayTest.byteNegate(array);
         assertEquals(array, new int[]{15, 7, 6, 1, 0, -1, -2, -3, -8, -9, -11, -33});
@@ -236,7 +235,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void testByteNegateNewArray() {
+    public void testByteNegateNewArray() {
         int[] array = new int[]{-16, -8, -7, -2, -1, 0, 1, 2, 7, 8, 10, 32};
         int[] firstResultArray = arrayTest.byteNegateNewArray(array);
         int[] secondResultArray = arrayTest.byteNegateNewArray(firstResultArray);
@@ -245,7 +244,7 @@ public class ArraysTest {
     }
 
     @Test
-    private void testSumPares() {
+    public void testSumPares() {
         int[] array1 = new int[]{1, 6, 2, 5, 3, 4};
         int[] array2 = new int[]{1, 6, 2, 5, 3, 4, 7};
         assertEquals(arrayTest.sumPares(array1), new int[]{7, 7, 7});
@@ -253,21 +252,23 @@ public class ArraysTest {
     }
 
     @Test
-    private void testIsEven() {
+    public void testIsEven() {
         assertEquals(arrayTest.isEven(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}), new boolean[]{false, true, false, true, false, true, false, true, false});
         assertEquals(arrayTest.isEven(new int[]{1, 5, 3}), new boolean[]{false, false, false});
         assertEquals(arrayTest.isEven(new int[]{2, 4, 6}), new boolean[]{true, true, true});
     }
 
     @Test
-    private void testFillCycle() {
+    public void testFillCycle() {
         assertEquals(arrayTest.fillCycle(5, 3), new int[]{3, 4, 5, 1, 2});
         assertEquals(arrayTest.fillCycle(8, 2), new int[]{7, 8, 1, 2, 3, 4, 5, 6});
         assertEquals(arrayTest.fillCycle(4, 1), new int[]{4, 1, 2, 3});
     }
 
     @Test
-    private void testCutLong() {
+    public void testCutLong() {
         assertEquals(arrayTest.cutLong(0b1110010110001110011000110011000111110011001100110111111100010101L), new int[]{0b11100101100011100110001100110001, 0b11110011001100110111111100010101});
+        assertEquals(arrayTest.cutLong(0b0000000000000000000000001100011011100000000000000000000000000111L), new int[]{0b00000000000000000000000011000110, 0b11100000000000000000000000000111});
+        assertEquals(arrayTest.cutLong(512L), new int[]{0, 512});
     }
 }
