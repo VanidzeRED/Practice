2641ed0b3079ee9d9f3e456ef17a936e4e2b9067
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2021-01-09 13:42:58 +0400

    Tests for methods in exception task
    All posible (maybe not all) exceptions throws are tested

--- src/test/java/ru/ssau/tk/pomelnikov/newtestproject/ExceptionsTaskTest.java
+++ src/test/java/ru/ssau/tk/pomelnikov/newtestproject/ExceptionsTaskTest.java
@@ -16,13 +16,30 @@ public class ExceptionsTaskTest {
 
     @Test
     public void testFindSymbol() {
+        assertEquals(exceptionsTask.findSymbol(new String[]{"qwerty", "asdFg", "zxcv", "string"}, 3), new char[]{'r', 'F', 'v', 'i'});
+        assertThrows(StringIndexOutOfBoundsException.class, () -> exceptionsTask.findSymbol(new String[]{"qwerty", "asdFg", "zxcv", "string"}, 4));
+        assertThrows(StringIndexOutOfBoundsException.class, () -> exceptionsTask.findSymbol(new String[]{"qwerty", "asdFg", "zxcv", "string"}, -1));
+        assertThrows(NullPointerException.class, () -> exceptionsTask.findSymbol(new String[]{null, "asdFg", "zxcv", "string"}, 4));
+        assertThrows(NullPointerException.class, () -> exceptionsTask.findSymbol(null, 10));
     }
 
     @Test
     public void testCharAt() {
+        assertEquals(exceptionsTask.charAt(new String[]{"qwerty", "asdFg", "zxcv", "string"}, 3, 2), 'r');
+        assertEquals(exceptionsTask.charAt(new String[]{"qwerty", "asdFg", "zxcv", "string"}, 1, 3), 'F');
+        assertThrows(NullPointerException.class, () -> exceptionsTask.charAt(null, 1, 2));
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> exceptionsTask.charAt(new String[]{"qwerty", "asdFg", "zxcv", "string"}, 4, 1));
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> exceptionsTask.charAt(new String[]{"qwerty", "asdFg", "zxcv", "string"}, 5, 10));
+        assertThrows(StringIndexOutOfBoundsException.class, () -> exceptionsTask.charAt(new String[]{"qwerty", "asdFg", "zxcv", "string"}, 0, 8));
     }
 
     @Test
     public void testDivision() {
+        assertEquals(exceptionsTask.division("54", "20"), 2);
+        assertEquals(exceptionsTask.division("100", "10"), 10);
+        assertThrows(NumberFormatException.class, () -> exceptionsTask.division(null, "2"));
+        assertThrows(NumberFormatException.class, () -> exceptionsTask.division("1", "aaa"));
+        assertThrows(NumberFormatException.class, () -> exceptionsTask.division("1", "0.5"));
+        assertThrows(ArithmeticException.class, () -> exceptionsTask.division("1", "0"));
     }
 }
