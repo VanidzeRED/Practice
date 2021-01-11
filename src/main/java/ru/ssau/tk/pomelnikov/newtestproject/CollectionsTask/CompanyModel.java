ed4e9cc4f83f36115b54276065bf122aa8c69f3c
Ivan Pomelnikov <70655147+VanidzeRED@users.noreply.github.com>
2021-01-11 16:08:20 +0400

    Adding methods fpr CompanyModel

--- src/main/java/ru/ssau/tk/pomelnikov/newtestproject/CollectionsTask/CompanyModel.java
+++ src/main/java/ru/ssau/tk/pomelnikov/newtestproject/CollectionsTask/CompanyModel.java
@@ -1,13 +1,15 @@
 package ru.ssau.tk.pomelnikov.newtestproject.CollectionsTask;
 
-import java.util.Collection;
-import java.util.LinkedHashSet;
+import ru.ssau.tk.pomelnikov.newtestproject.Gender;
+
+import java.util.*;
 
 public class CompanyModel {
 
     private final Collection<Location> allLocations;
     private final Collection<Route> allRoutes;
     private final Collection<Driver> allDrivers;
+    private int maxId = 0;
 
     public CompanyModel() {
         allLocations = new LinkedHashSet<>();
@@ -25,5 +27,48 @@ public class CompanyModel {
 
     public Collection<Driver> getAllDrivers() {
         return allDrivers;
+    }
+
+    public Settlement addSettlement(String name, double latitude, double longitude, SettlementType type, int population) {
+        Settlement settlement = new Settlement();
+        settlement.setId(++maxId);
+        settlement.setName(name);
+        settlement.setLatitude(latitude);
+        settlement.setLongitude(longitude);
+        settlement.setType(type);
+        settlement.setSettlement(population);
+        allLocations.add(settlement);
+        return settlement;
+    }
+
+    public Waypoint addWaypoint(String name, double latitude, double longitude, WaypointType type, Settlement settlement) {
+        Waypoint waypoint = new Waypoint();
+        waypoint.setId(++maxId);
+        waypoint.setName(name);
+        waypoint.setLatitude(latitude);
+        waypoint.setLongitude(longitude);
+        waypoint.setType(type);
+        waypoint.setSettlement(settlement);
+        allLocations.add(waypoint);
+        return waypoint;
+    }
+
+    public Driver addDriver(String name, Gender gender, Date licenseExpirationDate) {
+        Driver driver = new Driver();
+        driver.setId(++maxId);
+        driver.setName(name);
+        driver.setGender(gender);
+        driver.setLicenseExpirationDate(licenseExpirationDate);
+        allDrivers.add(driver);
+        return driver;
+    }
+
+    public Route addRoute(List<Location> locations) {
+        Route route = new Route();
+        for (Location location : locations) {
+            route.addLocation(location);
+        }
+        allRoutes.add(route);
+        return route;
     }
 }
